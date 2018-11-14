89d38ebbdacb3579f227ae289a7ec8bc63b447cb
Benedek László <lacbenedek@gmail.com>
2018-11-14 20:08:03 +0100

    CMD

--- WindowsFormsOpengl/Program.cs
+++ WindowsFormsOpengl/Program.cs
@@ -21,23 +21,23 @@ namespace OpenTKTest
                 foreach(string _arg in args)
                 {
                     string[] _args = _arg.Split(split.ToCharArray());
-                    if (_args[0] == "--file")
+                    if (_args[0] == "--file" || _args[0] == "-f")
                     {
                         CalculateMesh.GetFile(_args[1]);
                     }
-                    if (_args[0] == "--speed")
+                    if (_args[0] == "--rotate" || _args[0] == "-r")
                     {
                         Game.rotation_speed_x = float.Parse(_args[1]);
                         Game.rotation_speed_y = float.Parse(_args[2]);
                         Game.rotation_speed_z = float.Parse(_args[3]);
                     }
-                    if (_args[0] == "--help")
+                    if (_args[0] == "--help" || _args[0] == "-h")
                     {
                         Console.WriteLine("You can specify file with --file=[FILEPATH]");
                         Console.WriteLine("You can specify rotaition speed with --speed=[FLOAT]");
-                        Console.WriteLine("You can specify rotaition framerate cap with --framerate=[INT]");
+                        Console.WriteLine("You can specify framerate cap with --framerate=[INT]");
                     }
-                    if (_args[0] == "--framerate")
+                    if (_args[0] == "--framerate" || _args[0] == "-fps")
                     {
                         Game.framerate = int.Parse(_args[1]);
                     }
